633419ba8f01f1fbfc7d99af56144f24fecd773d
mbsurya19122004 <mbsurya19122004@gmail.com>
2026-09-14 17:47:01 +0530

    changed to ipv4 for send mail

--- Backend/utils/sendMail.js
+++ Backend/utils/sendMail.js
@@ -1,4 +1,3 @@
-
 require("dotenv").config();
 
 const nodemailer = require("nodemailer");
@@ -6,14 +5,15 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false, // STARTTLS
+    secure: false,
+    family: 4,
+
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     }
 });
 
-// Check SMTP connection when server starts
 transporter.verify((error, success) => {
     if (error) {
         console.error("❌ SMTP ERROR:", error);
@@ -37,7 +37,6 @@ const sendMail = async (to, subject, html) => {
 
     } catch (error) {
         console.error("❌ Mail Error:", error);
-
         throw error;
     }
 };
